27debb15d03692f736c8fd00bea09e27de1dafc2
Oliver Bernard Rozario <busycake@outlook.com>
2023-10-28 14:16:51 +0000

    Changes to be committed: 	modified:   config.h

--- config.h
+++ config.h
@@ -21,8 +21,8 @@ static const char *colors[][3]      = {
 
 
 /* tagging */
-static const char *tags[] = {  "dev", "www", "sys", "doc", "vbox", "chat", "mus", "vid", "gfx"  };
-// static const char *tags[] = { "󰇧 ", " ", " " , " ", " ", " ", " ", " ", " " };
+//static const char *tags[] = {  "dev", "www", "sys", "doc", "vbox", "chat", "mus", "vid", "gfx"  };
+static const char *tags[] = { "󰇧 ", " ", " " , " ", " ", " ", " ", " ", " " };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -42,8 +42,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "Tile",      tile },    /* first entry is default */
+	{ "Float",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
